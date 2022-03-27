39fa69318e700fe6ad46a6b4236b3853fcb8e38e
Jonas Holbech <jh@jonasholbech.dk>
2022-03-27 17:06:10 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import { NumberProvider } from "./contexts/mainContext";
+
 import Button from "./components/Button";
 function App() {
   return (
-    <NumberProvider>
-      <div className="App">
-        <h1>hello world</h1>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-      </div>
-    </NumberProvider>
+    <div className="App">
+      <h1>hello world</h1>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </div>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,29 @@
-import React, { useContext } from "react";
-import { ACTIONS, NumberContext } from "../contexts/mainContext";
+import React, { useReducer } from "react";
+
+export const ACTIONS = {
+  INCREASE: "increase",
+  DECREASE: "decrease",
+};
+const initialState = {
+  value: 0,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREASE:
+      return {
+        value: state.value + 1,
+      };
+    case ACTIONS.DECREASE:
+      return {
+        value: state.value - 1,
+      };
+    default:
+      return state;
+  }
+};
+
 function Button() {
-  const { state, dispatch } = useContext(NumberContext); // (*)  const onIncrease = () => dispatch({ type: ACTIONS.INCREASE });
+  const [state, dispatch] = useReducer(reducer, initialState);
   const onDecrease = () => dispatch({ type: ACTIONS.DECREASE });
   const onIncrease = () => dispatch({ type: ACTIONS.INCREASE });
   return (
