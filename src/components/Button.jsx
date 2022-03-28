8d3ac0318c3bd95b59f7c9c26cd955e0f4b61cd1
Jonas Holbech <jh@jonasholbech.dk>
2022-03-28 11:52:13 +0200

    remove comment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { NumberContext } from "../contexts/mainContext";
 function Button() {
-  const { count, setCount } = useContext(NumberContext); // (*)  const onIncrease = () => dispatch({ type: ACTIONS.INCREASE });
+  const { count, setCount } = useContext(NumberContext);
   return (
     <div>
       <button onClick={() => setCount((old) => old + 1)}>+1</button>
